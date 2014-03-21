335699524856b0d266cab5c5e68ba516a5b25699
Mitchell Foley <mitch.foley@mail.com>
2014-03-20 21:17:54 -0400

    Also grabs every beer

--- scraper.py
+++ scraper.py
@@ -1,10 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
+a=requests.get("http://www.thebeerstore.ca/beers/search/")
 b=requests.get("http://thebeerstore.ca/beers/sale")
+allbeer=a.text
+allsoup = BeautifulSoup(allbeer)
 beer=b.text
 bsoup=BeautifulSoup(beer)
 deal=[]
+regulars=[]
 f=open("sales.txt",'w')
 for i in bsoup.find_all('a'):
     s=i.text
@@ -17,6 +21,19 @@ for i in bsoup.find_all('a'):
                 deal.append(s.strip())
                 exit
 print "Done"
-print deal
 f.write(str(deal))
 f.close()
+g=open("regulars.txt",'w')
+for i in allsoup.find_all('a'):
+    s=i.text
+    if not(s.strip()==""):
+        used = False
+        for q in s:
+            if q == '%' and used==False:
+                used = True
+                print s.strip()
+                regulars.append(s.strip())
+                exit
+print "Done"
+g.write(str(regulars))
+g.close()
